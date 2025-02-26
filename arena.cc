7efd8c20f35312d1a332e27ccbd7c75cfa6bd230
ggp <georgii.galushko@gmail.com>
2025-02-26 19:58:41 +0300

    minor refactoring

--- arena.cc
+++ arena.cc
@@ -6,7 +6,7 @@
 
 static const u32 PageSize = 16*((u32)1 << 20);
 
-static u32 free(Page *p)
+static u32 free_size(Page *p)
 {
 	return p ? PageSize - ((u64)p->data - (u64)p) : 0;
 }
@@ -15,7 +15,7 @@ void *alloc(Arena &a, u32 size, u16 align)
 {
 	if (size + align > PageSize - sizeof(Page))
 		return 0;
-	if (free(a.p) < size + align) {
+	if (free_size(a.p) < size + align) {
 		Page *p = (Page *)mmap(0, PageSize, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
 		if (p == MAP_FAILED)
 			return 0;
