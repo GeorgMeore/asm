41ba79ee5aa634cde3c17d16b00bf35afca575a0
Georgii <georgii.galushko@gmail.com>
2026-06-18 22:03:28 +0400

    add a comment

--- arm64.cc
+++ arm64.cc
@@ -272,6 +272,8 @@ static Logical encode(u64 v, u8 vsize)
 
 static void push_logical(Inst &i, Logical l)
 {
+	// I actually wonder why arm chose such a fucked up way of
+	// encoding imms and not simply l.size | (l.ones - 1)
 	push_bits(i, ((~l.size ^ (l.size - 1)) & 0x3f) | (l.ones - 1) , 6);
 	push_bits(i, l.rot, 6);
 	push_bits(i, l.size == 64, 1);
